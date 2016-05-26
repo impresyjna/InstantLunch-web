6474ebf19a7ae56e80082abdad26b25b1547e136
wampir1408 <joanna.furmaniak@student.put.poznan.pl>
2016-05-27 01:03:26 +0200

    New and edit for dish

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -3,8 +3,6 @@ class DishesController < FrontController
     @dishes = Dish.where(restaurant_owner_id: current_user.actable_id)
   end
 
-  # GET /dishes/1
-  # GET /dishes/1.json
   def show
     respond_to do |format|
       format.html # show.html.erb
@@ -12,49 +10,40 @@ class DishesController < FrontController
     end
   end
 
-  # GET /dishes/new
   def new
     @dishes_categories = DishCategory.where(restaurant_owner_id: current_user.actable_id)
     @dishes_categories = @dishes_categories.pluck(:name, :id)
     @dish = Dish.new
   end
 
-  # GET /dishes/1/edit
   def edit
+    @dishes_categories = DishCategory.where(restaurant_owner_id: current_user.actable_id)
+    @dishes_categories = @dishes_categories.pluck(:name, :id)
+    @dish = Dish.find(params[:id])
   end
 
-  # POST /dishes
-  # POST /dishes.json
   def create
-    @dish = Dish.new(dish_params)
-
-    respond_to do |format|
-      if @dish.save
-        format.html { redirect_to @dish, notice: 'Dish was successfully created.' }
-        format.json { render json: @dish, status: :created }
-      else
-        format.html { render action: 'new' }
-        format.json { render json: @dish.errors, status: :unprocessable_entity }
-      end
+    @restaurant_owner = RestaurantOwner.find(current_user.actable_id)
+    @dish = @restaurant_owner.dishes.create(dish_params)
+    if @dish.save
+      flash[:success] = "Dodano potrawę"
+      redirect_to dishes_path
+    else
+      flash[:warning] = "Nie udało się dodać potrawy"
+      render 'new'
     end
   end
 
-  # PATCH/PUT /dishes/1
-  # PATCH/PUT /dishes/1.json
   def update
-    respond_to do |format|
-      if @dish.update(dish_params)
-        format.html { redirect_to @dish, notice: 'Dish was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @dish.errors, status: :unprocessable_entity }
-      end
+    @dish = Dish.find(params[:id])
+    if @dish.update_attributes(dish_params)
+      flash[:success] = "Dane zaktualizowane"
+      redirect_to dishes_path
+    else
+      render 'edit'
     end
   end
 
-  # DELETE /dishes/1
-  # DELETE /dishes/1.json
   def destroy
     @dish.destroy
     respond_to do |format|
@@ -64,13 +53,8 @@ class DishesController < FrontController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_dish
-      @dish = Dish.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def dish_params
-      params[:dish]
-    end
+  def dish_params
+    params.require(:dish).permit(:name, :price, :description, :dish_category_id)
+  end
 end
